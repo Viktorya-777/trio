906042b6ac98c7e8b1282a12ee097f40fdc0696e
Viktorya-777 <vikulya.kostenko.2018@mail.ru>
2021-05-19 18:24:53 +0300

    Deploy

--- public/script.js
+++ public/script.js
@@ -39,13 +39,12 @@ function initGallery(){
 function initForm(){
   // Это firebase конфиг, взятый из документации
   var firebaseConfig = {
-    apiKey: "AIzaSyCsxF4yZPMg2BbFXuwsODwq-hByW49QCzs",
-    authDomain: "trio-2fcaa.firebaseapp.com",
-    projectId: "trio-2fcaa",
-    storageBucket: "trio-2fcaa.appspot.com",
-    messagingSenderId: "1086937508512",
-    appId: "1:1086937508512:web:e5d1a87d6b8edaa39837c5",
-    measurementId: "G-W5TZW2R0DN"
+    apiKey: "AIzaSyD8cdBzG9rci9TBRYT1Ufc_6Og5IW6TLsk",
+    authDomain: "trio-2ccea.firebaseapp.com",
+    projectId: "trio-2ccea",
+    storageBucket: "trio-2ccea.appspot.com",
+    messagingSenderId: "634794841077",
+    appId: "1:634794841077:web:1d0b3a1bc6e044542bc6bc"
   };
   // Initialize Firebase
   // По документации - нужно сначала инициализировать ядро firebase
